6f56560d3a17ff5764b4e1c7142d8941f27c110a
Pierre Epron <pierre.epron@gmail.com>
2022-10-21 17:46:39 +0200

    move client behaviors in client module

--- tools/update_register.py
+++ tools/update_register.py
@@ -1,4 +1,5 @@
-from pymongo import MongoClient
+from client import *
+
 from tqdm import tqdm
 import requests
 import logging
@@ -15,80 +16,6 @@ ACL_ID_PATTERN = re.compile(r"url = \"(?:http(?:s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)\
 
 config.fileConfig('logging.conf')
 logger = logging.getLogger('updateRegister')
-
-
-def connect_mongo(url):
-    """
-        Connect to mongodb server.
-        Check if connection is alive (or wait timeout).
-        Log any errors.
-
-        Parameters
-        ----------
-        url : str, mongodb url to connect.
-
-        Returns
-        -------
-        client : pymongo.MongoClient, mongodb client with an active connection.
-    """
-    try:
-        client = MongoClient(f'{url}', connect=True)
-        client.is_mongos # Wait connection timeout if can't connect
-        logger.debug(f'connect mongodb at {MONGO_URL}')
-        return client
-    except Exception as e:
-        logger.exception(f'can\'t connect mongodb at {url}')
-        sys.exit()
-
-
-def get_db(client, name):
-    """
-        Get a mongodb database by name. 
-        Raise exception if not found.
-        Log any errors.
-
-        Parameters
-        ----------
-        client : pymongo.MongoClient, mongodb client with an active connection.
-        name : str, name of database to get.
-        
-        Returns
-        -------
-        db : pymongo.database.Database, mongodb database with an active connection.
-    """
-    try:
-        if name not in client.list_database_names():
-            raise
-        logger.debug(f'database {name} found')
-        return client[name]
-    except Exception as e:
-        logger.exception(f'database  `{name}` not found')
-        sys.exit()
-
-
-def get_collection(db, name):
-    """
-        Get a mongodb collection by name. 
-        Raise exception if not found.
-        Log any errors.
-
-        Parameters
-        ----------
-        db : pymongo.database.Database, mongodb database with an active connection.
-        name : str, name of collection to get.
-        
-        Returns
-        -------
-        collection : pymongo.collection.Collection, mongodb collection, with an active connection.
-    """
-    try:
-        if name not in db.list_collection_names():
-            raise
-        logger.debug(f'collection {name} found')
-        return db[name]    
-    except Exception as e:
-        logger.exception(f'collection  `{name}` not found')
-        sys.exit()
 
 
 def get_url_content(url):
