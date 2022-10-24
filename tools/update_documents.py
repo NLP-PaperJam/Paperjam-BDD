b8ff91734c4a16abda3437b778b4dd5bcf1e9461
Pierre Epron <pierre.epron@gmail.com>
2022-10-24 20:34:32 +0200

    improve update documents with batch handling

--- tools/update_documents.py
+++ tools/update_documents.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from enum import Enum
 from pathlib import Path
+from typing import Iterable
 from client import *
 import tempfile
 from grobid_client.grobid_client import GrobidClient
@@ -33,7 +34,7 @@ class StepCode(Enum):
 def get_batch(batch_size, register_filter):
     try:
         register_entries = list(register.find(register_filter, limit=batch_size))
-        batch = zip(register_entries, [{'acl_id':entry['acl_id']} for entry in register_entries])
+        batch = register_entries, [{'acl_id':entry['acl_id']} for entry in register_entries]
         logger.debug(f'success for get_batch with : batch_size = {batch_size}, register_filter = {register_filter}')
         return batch
     except:
@@ -41,95 +42,98 @@ def get_batch(batch_size, register_filter):
         sys.exit()
 
 
-def update_register_steps(register, entry, name, code, msg, close=False):
-    try:
-        now = datetime.now()
-        acl_id = entry['acl_id']
-        steps = entry['steps']
-        steps.append({'name':name, 'timestamp':now.timestamp(), 'code':code.value, 'msg':msg})
-        # TODO : If step already exist raise error
-        register.update_one({'acl_id':acl_id}, {'$set':{'close':close, 'steps':steps}})
-        logger.debug(f'success for update_register_steps with : acl_id = {acl_id}, close = {close}, new_step = {steps[-1]}')
-    except:
-        logger.exception(f'failure for update_register_steps with : acl_id = {acl_id}, close = {close}, new_step = {steps[-1]}')
+def update_register_steps(entry, name, code, msg, close=False):
+    now = datetime.now()
+    # TODO : If step already exist raise error
+    entry['steps'].append({'name':name, 'timestamp':now.timestamp(), 'code':code.value, 'msg':msg})
+    entry['close'] = close
 
 
-def get_s2_api(entry, document, url, fields):
-
-    acl_id = entry['acl_id']
-    name = 's2_api'
-    code = StepCode.SUCCESS
-    msg = ''
-    close = False
-
-    try:
-        url = f'{url}ACL:{acl_id}?fields={fields}'
-        res = requests.get(url) # TODO : move url
-        if res.status_code == 200:
-            document['s2'] = res.json()
-            logger.debug(f'success for get_s2_api with : acl_id = {acl_id}, url = {res.url}')
-            msg = '200'
-        elif res.status_code == 404:
-            logger.warning(f'404 for get_s2_api with : acl_id = {acl_id}, url = {res.url}')
-            code = StepCode.TRASHED
-            close = True
-            msg = res.json()['error']
-        else :
-            logger.error(f'{res.status_code} for get_s2_api with : acl_id = {acl_id}, url = {res.url}')
-            code = StepCode.ERROR
-            close = True
-            msg = res.json()['error']
-    except Exception as e:
-        logger.exception(f'failure for get_s2_api with : acl_id = {acl_id}, url = {res.url}')
-        code = StepCode.ERROR
-        close = True
-        msg = 'Unknow exception triggered. Check logs.'
-    finally:
-        update_register_steps(register, entry, name, code, msg, close)
-        return code
-
-def get_acl_pdf(entry, document, dir_path : Path, url):
-
-    acl_id = entry['acl_id']
-    name = 'acl_pdf'
-    code = StepCode.SUCCESS
-    msg = ''
-    close = False
-
-    try:
-        url = f'{url}/{acl_id}.pdf'
-        res = requests.get(url) # TODO : move url
-        if res.status_code == 200:
-            (dir_path / f'{acl_id}.pdf').write_bytes(res.content)
-            logger.debug(f'success for get_acl_pdf with : acl_id = {acl_id}, dir_path = {dir_path}, url = {url}')
-        elif res.status_code == 404:
-            logger.warning(f'404 for get_acl_pdf with : acl_id = {acl_id}, dir_path = {dir_path}, url = {url}')
-            code = StepCode.TRASHED
-            close = True
-        else :
-            logger.error(f'{res.status_code} for get_acl_pdf with : acl_id = {acl_id}, dir_path = {dir_path}, url = {url}')
-            code = StepCode.ERROR
-            close = True
-        msg = str(res.status_code)
-    except Exception as e:
-        logger.exception(f'failure for get_acl_pdf with : acl_id = {acl_id}, dir_path = {dir_path}, url = {url}')
-        code = StepCode.ERROR
-        close = True
-        msg = 'Unknow exception triggered. Check logs.'
-    finally:
-        update_register_steps(register, entry, name, code, msg, close)
-        return code
-
-
-def post_grobid_api(entry : dict, document : dict, config_path : Path, dir_path : Path):    
+def get_s2_api(batch, url : str, fields : str):
     
-    acl_id = entry['acl_id']
+    for entry, document in zip(*batch):
+        
+        if entry['close']:
+            continue
+
+        try:
+            acl_id = entry['acl_id']
+            name = 's2_api'
+            code = StepCode.SUCCESS
+            msg = ''
+            close = False
+
+            res = requests.get(f'{url}ACL:{acl_id}?fields={fields}')  # TODO : move url
+            if res.status_code == 200:
+                document['s2'] = res.json()
+                logger.debug(f'success for get_s2_api with : acl_id = {acl_id}')
+                msg = '200'
+            elif res.status_code == 404:
+                logger.warning(f'404 for get_s2_api with : acl_id = {acl_id}')
+                code = StepCode.TRASHED
+                close = True
+                msg = res.json()['error']
+            else :
+                logger.error(f'{res.status_code} for get_s2_api with : acl_id = {acl_id}')
+                code = StepCode.ERROR
+                close = True
+                msg = res.json()['error'] if 'error' in res.json() else 'Unknow status code triggered. Check logs.'
+        except Exception as e:
+            logger.exception(f'failure for get_s2_api with : acl_id = {acl_id}')
+            code = StepCode.ERROR
+            close = True
+            msg = 'Unknow exception triggered. Check logs.'
+        finally:
+            update_register_steps(entry, name, code, msg, close)
+
+
+def get_acl_pdf(batch : Iterable, dir_path : Path, url : str):
+    name = 'acl_pdf'
+
+    for entry, _ in zip(*batch):
+        
+        if entry['close']:
+            continue
+
+        try:
+            acl_id = entry['acl_id']
+            code = StepCode.SUCCESS
+            msg = ''
+            close = False
+
+            res = requests.get(f'{url}/{acl_id}.pdf') # TODO : move url
+            if res.status_code == 200:
+                (dir_path / f'{acl_id}.pdf').write_bytes(res.content)
+                logger.debug(f'success for get_acl_pdf with : acl_id = {acl_id}, dir_path = {dir_path}, url = {url}')
+            elif res.status_code == 404:
+                logger.warning(f'404 for get_acl_pdf with : acl_id = {acl_id}, dir_path = {dir_path}, url = {url}')
+                code = StepCode.TRASHED
+                close = True
+            else :
+                logger.error(f'{res.status_code} for get_acl_pdf with : acl_id = {acl_id}, dir_path = {dir_path}, url = {url}')
+                code = StepCode.ERROR
+                close = True
+            msg = str(res.status_code)
+        except Exception as e:
+            logger.exception(f'failure for get_acl_pdf with : acl_id = {acl_id}, dir_path = {dir_path}, url = {url}')
+            code = StepCode.ERROR
+            close = True
+            msg = 'Unknow exception triggered. Check logs.'
+        finally:
+            update_register_steps(entry, name, code, msg, close)
+
+
+def post_grobid_api(batch : Iterable, config_path : Path, dir_path : Path):    
     name = 'grobid_api'
-    code = StepCode.SUCCESS
-    close = False
-    msg = ''
+
+    if len(list(dir_path.glob('*.pdf'))) == 0:
+        return
 
     try:
+        code = StepCode.SUCCESS
+        close = False
+        msg = ''
+
         grobid_client = GrobidClient(config_path=config_path)
         grobid_client.process( # TODO : Move configuration
             "processFulltextDocument", 
@@ -141,26 +145,44 @@ def post_grobid_api(entry : dict, document : dict, config_path : Path, dir_path 
             include_raw_affiliations=True,
             include_raw_citations=True
         )
-
-        if (dir_path / f'{acl_id}.tei.xml').is_file():
-            # TODO : should we test file content is parsable (bs4, lxml ...)
-            document['grobid'] = (dir_path / f'{acl_id}.tei.xml').read_text(encoding='utf-8')
-            msg = '200'
-            logger.debug(f'success for post_grobid_api with : acl_id = {acl_id}, dir_path = {dir_path}, url = {config_path}')
-        elif (dir_path / f'{acl_id}.txt').is_file():
-            msg = (dir_path / f'{acl_id}.tei.xml').read_text(encoding='utf-8') 
-            raise
-        else:
-            msg = f'file not found : {acl_id}(.tei.xml|.txt)'
-            raise
     except:
-        logger.exception(f'failure for post_grobid_api with : acl_id = {acl_id}, dir_path = {dir_path}, url = {config_path}')
+        logger.exception(f'failure for post_grobid_api with : dir_path = {dir_path}, url = {config_path}')
         code = StepCode.ERROR
         close = True
-        msg = 'Unknow exception triggered. Check logs.' if msg == '' else msg
-    finally:
-        update_register_steps(register, entry, name, code, msg, close)
-        return code
+        msg = 'Unknow exception triggered during grobid processing. Check logs.' if msg == '' else msg
+        for entry in batch[0]:
+            update_register_steps(entry, name, code, msg, close)
+        return
+
+    for entry, document in zip(*batch):
+
+        if entry['close']:
+            continue
+
+        try:
+
+            acl_id = entry['acl_id']
+            close = False
+            msg = ''
+
+            if (dir_path / f'{acl_id}.tei.xml').is_file():
+                # TODO : should we test file content is parsable (bs4, lxml ...)
+                document['grobid'] = (dir_path / f'{acl_id}.tei.xml').read_text(encoding='utf-8')
+                logger.debug(f'success for post_grobid_api with : acl_id = {acl_id}, dir_path = {dir_path}, url = {config_path}')
+            elif (dir_path / f'{acl_id}.txt').is_file():
+                msg = (dir_path / f'{acl_id}.tei.xml').read_text(encoding='utf-8') 
+                raise
+            else:
+                msg = f'file not found : {acl_id}(.tei.xml|.txt)'
+                raise
+        except:
+            logger.exception(f'failure for post_grobid_api with : acl_id = {acl_id}, dir_path = {dir_path}, url = {config_path}')
+            code = StepCode.ERROR
+            close = True
+            msg = 'Unknow exception triggered during grobid result handling. Check logs.' if msg == '' else msg
+        finally:
+            update_register_steps(entry, name, code, msg, close)
+
 
 if __name__ == '__main__':
     
@@ -173,17 +195,25 @@ if __name__ == '__main__':
     logger.info(f'create register entries batch ...')
     batch = get_batch(BATCH_DEFAULT_SIZE, BATCH_DEFAULT_FILTER)
 
-    entry, doc = list(batch)[0]
-    url = S2_API_URL
-    fields = S2_API_FIELDS
+    logger.info(f'start requesting s2 api ...')
+    get_s2_api(batch, S2_API_URL, S2_API_FIELDS)
 
-    get_s2_api(entry, doc, S2_API_URL, S2_API_FIELDS)
     with tempfile.TemporaryDirectory() as tmp:
-        get_acl_pdf(entry, doc, Path(tmp), ACL_PDF_URL)
-        post_grobid_api(entry, doc, Path('tools/grobid_config.json'), Path(tmp))
-    # update_register_steps(register, entry, 's2', StepCode.SUCCESS, '')
-    # register.update_one({'acl_id':entry['acl_id']}, ) 
+        logger.info(f'start downloading acl pdf ...')
+        get_acl_pdf(batch, Path(tmp), ACL_PDF_URL)
+        logger.info(f'start requesting grobid api ...')
+        post_grobid_api(batch, Path('tools/grobid_config.json'), Path(tmp))
 
+    logger.info(f'start updating database ...')
+
+    docs = [doc for entry, doc in zip(*batch) if entry['close'] == False]
+    insert_many(docs, documents)
+    logger.debug(f'success for insert documents batch ({len(docs)}/{BATCH_DEFAULT_SIZE}) ...')
+    
+    for entry in batch[0]:
+        update_one({'acl_id':entry['acl_id']}, {'$set': {'close':True, 'steps':entry['steps']}}, register)
+    logger.debug(f'success updating register entries ...')
+    
 
     register.database.client.close()
     logger.info(f'all process ended')
