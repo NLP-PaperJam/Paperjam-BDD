9811b65e6c10dbadb840a9cb9553959b02eeeef9
Pierre Epron <pierre.epron@gmail.com>
2022-11-17 18:31:58 +0100

    add MONGO constant

--- tools/client.py
+++ tools/client.py
@@ -11,6 +11,10 @@ import os, sys
 config.fileConfig('logging.conf')
 logger = logging.getLogger('mongoClient')
 
+MONGO_DB_NAME = os.getenv('MONGO_DB_NAME', 'pwj-db')
+MONGO_REGISTER_COLLECTION = os.getenv('MONGO_REGISTER_COLLECTION', 'register')
+MONGO_DOCUMENTS_COLLECTION = os.getenv('MONGO_DOCUMENTS_COLLECTION', 'documents')
+
 def connect_mongo():
     """
         Connect to mongodb server.
